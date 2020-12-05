30a330fd0e795a157ea1feee356b39af35cccf49
max <me@mclifford.dev>
2020-12-05 17:01:44 +0000

    Added simple data handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 use std::net::{TcpStream, TcpListener};
 use crate::logging::{Log, Logger, LogItem};
-use crate::common::RequestSettings;
+use crate::common::{RequestSettings, CorrelationId};
 use crate::messaging::*;
 
 pub mod common;
@@ -13,27 +15,26 @@ pub mod messaging;
 
 
 pub fn listen(ip_address: &str) {
-
     let (log, logger) = Log::create();
-    
+
     let pool = threads::ThreadPool::new(4, logger.clone());
 
     let listener = TcpListener::bind(ip_address);
 
     let thread_logger = logger.clone();
-    
+
     match (pool, listener) {
         (Ok(pool), Ok(listener)) => {
             for stream in listener.incoming() {
                 let stream = stream.unwrap();
-                
+
                 let l = thread_logger.clone();
-                
+
                 pool.execute(|| {
                     handle_connection(stream, l);
                 })
             }
-        },
+        }
         (Err(e1), Err(e2)) => {
             // Both thread pool and listener could failed.
             println!("e1: {}, e2: {}", e1, e2);
@@ -55,18 +56,34 @@ fn handle_connection(mut stream: TcpStream, logger: Logger) {
 
     // Attempted to handled the handshake
     let handshake_result = handle_handshake(&stream);
-    
+
     match handshake_result {
         Ok(settings) => {
-            let result = handle_request(&stream, settings);
-            
-            // TOOD handle the result.
+            let result = handle_request(&stream, &settings);
+
+            // TODO handle error
+            match result {
+                Ok(message) => {
+                    let data = message.get_data();
+
+                    handle_data(settings.correlation_id.clone(), data);
+                    
+                    let message = 
+                        format!("Message received from `{}`. Correlation ID: {}, Frames: {}", 
+                                settings.from.value,
+                                settings.correlation_id.value,
+                                settings.frame_count);
+
+                    // For now save the sa
+                    
+                    logger.send(LogItem::info(String::from("connection_handler"), message));
+                }
+                Err(e) => {}
+            }
         }
-        Err (e) => {
-            
-        }
+        Err(e) => {}
     }
-    
+
     stream.read(&mut buffer).unwrap();
 }
 
@@ -81,41 +98,39 @@ fn handle_handshake(mut stream: &TcpStream) -> Result<RequestSettings, &'static 
 
             let response = header.create_response();
             let settings = header.create_settings();
-            
+
             let write_result = stream.write(&response.to_bytes());
-            
+
             match write_result {
                 Ok(_) => Ok(settings),
                 Err(_) => Err("Could not write handshake response")
             }
         }
         Err(_) => Err("Could not read handshake header")
-        
     }
-    
 }
 
-fn handle_request(mut stream: &TcpStream, settings: RequestSettings) -> Result<MessageResult, &'static str> {
+fn handle_request(mut stream: &TcpStream, settings: &RequestSettings) -> Result<MessageResult, &'static str> {
     let buffer = [0; 1024];
-    
-    let mut msg_result = MessageResult::create(settings.correlation_id, settings.frame_count as usize);
-    
+
+    let mut msg_result = MessageResult::create(settings.correlation_id.clone(), settings.frame_count as usize);
+
     for i in 0..settings.frame_count {
         let frame = read_frame(stream, buffer);
-        
+
         let result = match handle_frame(&frame) {
             Ok(_) => {
-                FrameResult::create_success(frame.header.frame_number, frame) 
+                FrameResult::create_success(frame.header.frame_number, frame)
             }
             Err(e) => {
                 FrameResult::create_error(frame.header.frame_number, e)
             }
         };
-        
+
         msg_result.add_result(result);
     }
-    
-    Ok (msg_result)
+
+    Ok(msg_result)
 }
 
 fn read_frame(mut stream: &TcpStream, mut buffer: [u8; 1024]) -> Frame {
@@ -126,4 +141,23 @@ fn read_frame(mut stream: &TcpStream, mut buffer: [u8; 1024]) -> Frame {
 
 fn handle_frame(frame: &Frame) -> Result<(), &'static str> {
     Err("Not implemented")
+}
+
+fn handle_data(cor_id: CorrelationId, data: Vec<u8>) {
+    // For now just save the data to a fixed location.
+    let location = "/home/max/Data/rustik_test";
+    
+    
+    
+    let path = Path::new(&cor_id.value);
+    
+    let mut file = match File::create(&path) {
+        Err(_) => panic!("Could not create file."),
+        Ok(file) => file,
+    };
+    
+    match file.write_all(&data) {
+        Err(_) => panic!("Could not save to file"),
+        Ok(_) => (),
+    }
 }
--- src/messaging.rs
+++ src/messaging.rs
@@ -16,12 +16,12 @@ pub struct HandshakeResponseHeader {
 
 pub struct Frame {
     pub header: FrameHeader,
-    data: [u8; 1016] // While fixed sized framesize/buffer is used this can be set to buffer size - header size or 1024 - 8.
+    data: [u8; 1016], // While fixed sized framesize/buffer is used this can be set to buffer size - header size or 1024 - 8.
 }
 
 pub struct FrameHeader {
-    correlation_id: CorrelationId,
-    frame_number: u16,
+    pub correlation_id: CorrelationId,
+    pub frame_number: u16,
 }
 
 pub struct FrameResponseHeader {
@@ -29,16 +29,31 @@ pub struct FrameResponseHeader {
     frame_number: u16,
 }
 
+pub struct MessageResult {
+    pub correlation_id: CorrelationId,
+    pub frame_results: Vec<FrameResult>,
+}
+
+pub enum FrameResultType {
+    Success(Frame),
+    Error(&'static str),
+}
+
+pub struct FrameResult {
+    pub number: u16,
+    pub result: FrameResultType,
+}
+
+
 impl HandshakeHeader {
     pub fn create(data: [u8; 32]) -> HandshakeHeader {
-
         let from = NodeId::create([data[0], data[1], data[2], data[3]]);
 
         let frame_size = create_u16([data[4], data[5]]);
 
         let frame_count = create_u16([data[6], data[7]]);
 
-        let correlation_id = CorrelationId::create([data[8],data[9],data[10],data[11],data[12],data[13]]);
+        let correlation_id = CorrelationId::create([data[8], data[9], data[10], data[11], data[12], data[13]]);
 
         let flags = [data[14], data[15]];
 
@@ -59,23 +74,27 @@ impl HandshakeHeader {
     }
 
     pub fn create_settings(&self) -> RequestSettings {
-        RequestSettings { frame_size: self.frame_size, frame_count: self.frame_count }
+        RequestSettings {
+            frame_size: self.frame_size,
+            frame_count: self.frame_count,
+            correlation_id: self.correlation_id.clone(),
+            from: self.from.clone(),
+        }
     }
 }
 
 impl HandshakeResponseHeader {
     pub fn create(data: [u8; 8]) -> HandshakeResponseHeader {
-        let correlation_id = CorrelationId::create([data[0],data[1],data[2],data[3],data[4],data[5]]);
-        let flags = [data[6],data[7]];
+        let correlation_id = CorrelationId::create([data[0], data[1], data[2], data[3], data[4], data[5]]);
+        let flags = [data[6], data[7]];
 
         HandshakeResponseHeader {
             correlation_id,
-            flags
+            flags,
         }
     }
 
     pub fn to_bytes(&self) -> [u8; 8] {
-
         let cor_id = self.correlation_id.to_bytes();
 
         let mut result: [u8; 8] = [0; 8];
@@ -92,7 +111,6 @@ impl HandshakeResponseHeader {
 }
 
 impl Frame {
-    
     /// Create a frame from a buffer.
     /// 
     /// ## Arguments
@@ -106,41 +124,88 @@ impl Frame {
         // Take the first 8 bytes 
         let mut header_buffer: [u8; 8] = [0; 8];
         let mut data_buffer: [u8; 1016] = [0; 1016];
-        
+
         for i in 0..7 {
-          header_buffer[i] = data[i];      
+            header_buffer[i] = data[i];
         };
-        
+
         let header = FrameHeader::create(header_buffer);
-        
+
         for i in 8..1023 {
             data_buffer[i] = data[i];
         }
-        
+
         Frame { header, data: data_buffer }
     }
 }
 
 impl FrameHeader {
     pub fn create(data: [u8; 8]) -> FrameHeader {
-        let correlation_id = CorrelationId::create([data[0],data[1],data[2],data[3],data[4],data[5]]);
-        let frame_number = create_u16([data[6],data[7]]);
+        let correlation_id = CorrelationId::create([data[0], data[1], data[2], data[3], data[4], data[5]]);
+        let frame_number = create_u16([data[6], data[7]]);
 
         FrameHeader {
             correlation_id,
-            frame_number
+            frame_number,
         }
     }
 }
 
 impl FrameResponseHeader {
     pub fn create(data: [u8; 8]) -> FrameResponseHeader {
-        let correlation_id = CorrelationId::create([data[0],data[1],data[2],data[3],data[4],data[5]]);
-        let frame_number = create_u16([data[6],data[7]]);
+        let correlation_id = CorrelationId::create([data[0], data[1], data[2], data[3], data[4], data[5]]);
+        let frame_number = create_u16([data[6], data[7]]);
 
         FrameResponseHeader {
             correlation_id,
-            frame_number
+            frame_number,
+        }
+    }
+}
+
+impl MessageResult {
+    pub fn create(cor_id: CorrelationId, frame_count: usize) -> MessageResult {
+        MessageResult {
+            correlation_id: cor_id,
+            frame_results: Vec::with_capacity(frame_count),
+        }
+    }
+
+    pub fn add_result(&mut self, result: FrameResult) {
+        self.frame_results.push(result);
+    }
+    
+    pub fn get_data(&self) -> Vec<u8> {
+        
+        let mut data:Vec<u8> = Vec::new();
+        
+        for i in &self.frame_results {
+            match &i.result {
+                FrameResultType::Success(f) => {
+                    data.append(&mut f.data.to_vec());
+                }
+                FrameResultType::Error(e) => {
+                    // TODO Handle errors.
+                }
+            }
+        }
+        
+        data
+    }
+}
+
+impl FrameResult {
+    pub fn create_success(number: u16, frame: Frame) -> FrameResult {
+        FrameResult {
+            number,
+            result: FrameResultType::Success(frame),
+        }
+    }
+
+    pub fn create_error(number: u16, error: &'static str) -> FrameResult {
+        FrameResult {
+            number,
+            result: FrameResultType::Error(error),
         }
     }
 }
